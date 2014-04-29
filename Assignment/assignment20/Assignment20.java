0fde9764eebdfd125fc14f14733b4b6adfa93a29
zwxfzzzjr <zwxfzzzjr@gmail.com>
2014-04-29 10:54:07 -0600

    java

--- Assignment/assignment20/Assignment20.java
+++ Assignment/assignment20/Assignment20.java
@@ -2,27 +2,22 @@ package assignment20;
 
 public class Assignment20 {
 	public static void main(String[] args) {
-		System.out.println("*****array1******");
+		System.out.println("*****array1******");// print array method
 		int[] array1 = { 1, 2, 6, 4, 2, 2, 1, 5 };
 		ArrayUtility.print(array1);
 
-		System.out.println("*****array2******");
-
+		System.out.println("*****array2******");// randomize method
 		int[] array2 = new int[20];
 		ArrayUtility.randomize(array2, 100, 200);
 		ArrayUtility.print(array2);
 
-		System.out.println("*****array3******");
+		System.out.println("*****array3******");// increase method
 		int[] array3 = new int[7];
 		ArrayUtility.randomize(array3, 7, 10);
-
 		System.out.println("before:");
 		ArrayUtility.print(array3);
-
-		ArrayUtility.increase(array3, 10);
-
 		System.out.println("after:");
-		ArrayUtility.print(array3);
+		ArrayUtility.print(ArrayUtility.increase(array3, 10));
 
 	}
 }
