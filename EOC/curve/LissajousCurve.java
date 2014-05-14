9d5beae10f93a51db3fac4fd945f827f51a0fc26
zwxfzzzjr <zwxfzzzjr@gmail.com>
2014-05-14 00:41:48 -0600

    java

--- EOC/curve/LissajousCurve.java
+++ EOC/curve/LissajousCurve.java
@@ -2,22 +2,29 @@ package curve;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import util.MyFrame;
+import java.text.DecimalFormat;
+import util.FrameUtil;
+import util.MyFrameFast;
 
-public class LissajousCurve extends MyFrame {
+public class LissajousCurve extends MyFrameFast {
 	public static int width = 750;
 	public static int height = 750;
 
-	double k = 2;
+	double n = 3;
+	double delta = 0.025;
 	int number = 1000;
 
 	public void paint(Graphics g) {
 		g.fillRect(0, 0, 750, 750);
 		g.setColor(Color.red);
 		for (double i = 0; i <= 2 * Math.PI; i += 1 / (double) number)
-			g.fillOval((int) (300 * Math.sin(k * i)) + 375, (int) (300 * Math.cos((k + 1) * i)) + 375, 7, 7);
-		k += 0.025;
+			g.fillOval((int) (300 * Math.sin(n * i)) + 375, (int) (300 * Math.cos((n + 1) * i)) + 375, 7, 7);
+		n += delta;
+		if (n > 10 || n < -10)
+			delta = -delta;
 		number++;
+
+		FrameUtil.printInfo(g, "n = " + new DecimalFormat("0.000").format(n), 20, 75, 60, Color.white);
 	}
 
 	public static void main(String[] args) {
