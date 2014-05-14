d7640e85d43ba21100632db3fb27ffd9f5e6a1fa
zwxfzzzjr <zwxfzzzjr@gmail.com>
2014-05-14 00:12:13 -0600

    java

--- EOC/snake/GameFrame.java
+++ EOC/snake/GameFrame.java
@@ -1,15 +1,26 @@
 package snake;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import util.MyFrame;
 
 public class GameFrame extends MyFrame {
-	double k = 2;
-	int number = 1000;
+	Square a = new Square(Color.red, height / 2, width / 2, 25, 25);
+	Square b = new Square(Color.green, (int) (Math.random() * height), (int) (Math.random() * width), 25, 25);
 
 	public void paint(Graphics g) {
 		g.fillRect(0, 0, 750, 750);
+		a.draw(g);
+		b.draw(g);
+		if (a.getRect().intersects(b.getRect())) {
+			b = new Square(Color.green, random(50, height - 50), random(50, width - 50), 25, 25);
+		}
+	}
 
+	private int random(int low, int high) {
+		return (int) (Math.random() * (high - low + 1) + low);
 	}
 
 	public static void main(String[] args) {
@@ -18,6 +29,17 @@ public class GameFrame extends MyFrame {
 
 	public void launchFrame() {
 		super.launchFrame();
-
+		addKeyListener(new KeyMonitor());
 	}
+
+	class KeyMonitor extends KeyAdapter {
+		public void keyPressed(KeyEvent e) {
+			a.keyPressed(e);
+		}
+
+		public void keyReleased(KeyEvent e) {
+			a.keyReleased(e);
+		}
+	}
+
 }
