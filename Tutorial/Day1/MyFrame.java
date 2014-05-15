710cba2c8bc4fe64b4fc86a6cea4e03bdcb8e925
zwxfzzzjr <zwxfzzzjr@gmail.com>
2014-05-15 10:29:03 -0600

    The frame file

--- Tutorial/Day1/MyFrame.java
+++ Tutorial/Day1/MyFrame.java
@@ -4,6 +4,10 @@ package Day1;
 // You'll use it later
 import java.awt.Frame;
 
+// Those lines below imports the class to close your window
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 // I called it MyFrame, you can call it whatever you want
 // You have to extend it to show the window
 public class MyFrame extends Frame {
---
+++ Tutorial/Day2/MyFrame.java
@@ -0,0 +1,39 @@
+package Day2;
+
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class MyFrame extends Frame {
+	public void launchFrame() {
+		setSize(750, 750);
+		setLocation(100, 100);
+		setVisible(true);
+
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+	}
+
+	// paint method let you paint something on the window
+	public void paint(Graphics g) {
+		// g.setColor(Color.blue);
+		// g.setColor(Color.green);
+
+		// g.drawLine(x1, y1, x2, y2);
+		// g.drawString(str, x, y);
+
+		// g.drawOval(x, y, width, height);
+		// g.drawRect(x, y, width, height);
+		// g.drawArc(x, y, width, height, startAngle, arcAngle);
+		// g.drawRoundRect(x, y, width, height, arcWidth, arcHeight);
+
+		// g.fillOval(x, y, width, height);
+		// g.fillRect(x, y, width, height);
+		// g.fillArc(x, y, width, height, startAngle, arcAngle);
+		// g.fillRoundRect(x, y, width, height, arcWidth, arcHeight);
+	}
+}
