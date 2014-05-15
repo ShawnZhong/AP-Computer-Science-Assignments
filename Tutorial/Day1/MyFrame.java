978ece78047d3d7de5a51c290c7760107351afe7
zwxfzzzjr <zwxfzzzjr@gmail.com>
2014-05-14 18:34:35 -0600

    The frame file

--- Tutorial/Day1/MyFrame.java
+++ Tutorial/Day1/MyFrame.java
@@ -1,7 +1,5 @@
 package Day1;
 
-//TODO : Build a window
-
 // "awt" stands for Abstract Window Toolkit
 // You'll use it later
 import java.awt.Frame;
@@ -12,12 +10,22 @@ public class MyFrame extends Frame {
 	public void launchFrame() {
 
 		// set the size (in pixel) of your frame
-		setSize(500, 500);
+		setSize(100, 100);
 
 		// set the location (in pixel) of your frame
-		setLocation(100, 100);
+		setLocation(0, 0);
 
-		// set visible to true
-		setVisible(true);
+		// set visible
+		setVisible(false);
+
+		// uncomment these code below so that you are able to close your window
+
+		/*
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		*/
 	}
 }
